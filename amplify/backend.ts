62472820851ac402cc56664918cbb7c4f3be6b51
AidanMDB <121575554+AidanMDB@users.noreply.github.com>
2025-02-04 11:34:46 -0500

    -setup aws backend resources -storage (S3) -database (dynamoDB) -compute (lambda) -auth (cognito) -added name to README

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,16 @@
+import { myVidMakerFunction } from './functions/vid-maker/resource';
+import { myUploadFunction } from './functions/upload/resource';
 import { defineBackend } from '@aws-amplify/backend';
+import { storage } from './storage/resource';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 defineBackend({
   auth,
   data,
+  storage,
+  myUploadFunction,
+  myVidMakerFunction,
 });
+
+
