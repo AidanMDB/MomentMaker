d243c5a0d3ac9566769021a7dae0ef6eaaebf311
npeter <nilayahsp@gmail.com>
2025-02-25 15:44:07 -0500

    fix s3 permissions preventing upload

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,7 +4,7 @@ export const storage = defineStorage({
     name: 'MediaStorage',
     access: (allow) => ({
         'user-media/*': [
-            allow.entity('identity').to(['read', 'write', 'delete'])
+            allow.guest.to(['read', 'write', 'delete'])
         ],
     }),
 
