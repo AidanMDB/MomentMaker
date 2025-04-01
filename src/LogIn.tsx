130157ba7b0b2d5a5c1c1ae331867d0e71d3305f
npeter <nilayahsp@gmail.com>
2025-04-01 13:00:14 -0400

    stylize login in page

--- src/LogIn.tsx
+++ src/LogIn.tsx
@@ -1,23 +1,28 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import './LogIn.css'
 import { useNavigate } from 'react-router-dom';
 
 export default function LogIn() {
   const navigate = useNavigate();
 
   return (
-    <Authenticator>
-      {({ user }) => {
-        if (user) {
-          navigate("/all");
-        }
+    <ThemeProvider>
+      <div className="login-container">
+        <Authenticator>
+          {({ user }) => {
+            if (user) {
+              navigate("/all");
+            }
 
-        return (
-          <div>
-            <h1>Welcome, {user?.username}!</h1>
-          </div>
-        );
-      }}
-    </Authenticator>
+            return (
+              <div>
+                <h1>Welcome, {user?.username}!</h1>
+              </div>
+            );
+          }}
+        </Authenticator>
+      </div>
+    </ThemeProvider>
   );
 }
