4da9a21b7048dc8b8a20ecffdc3605d4931399f7
rileythomas529 <thom1028@purdue.edu>
2025-04-22 14:22:07 -0400

    comments to fix lint

--- amplify/functions/videoStarter/handler.ts
+++ amplify/functions/videoStarter/handler.ts
@@ -1,24 +1,24 @@
-import { RekognitionClient } from '@aws-sdk/client-rekognition';
+//import { RekognitionClient, StartFaceDetectionCommand } from '@aws-sdk/client-rekognition';
 import { Handler } from 'aws-lambda';
 //import { v4 as uuidv4 } from 'uuid';
 
-const rekogClient = new RekognitionClient();
+//const rekogClient = new RekognitionClient();
 
 export const handler: Handler = async (event) => {
     console.log('Event: ', event);
     
-    const params = {
-        Video: {
-            S3Object: {
-                Bucket: event.bucket,
-                Name: event.key
-            }
-        },
-        NotificationChannel: {
-            SNSTopicArn: process.env.SNS_TOPIC_ARN,
-            RoleArn: process.env.REKOGNITION_ROLE_ARN
-        }
-    };
+    // const params = {
+    //     Video: {
+    //         S3Object: {
+    //             Bucket: event.bucket,
+    //             Name: event.key
+    //         }
+    //     },
+    //     NotificationChannel: {
+    //         SNSTopicArn: process.env.SNS_TOPIC_ARN,
+    //         RoleArn: process.env.REKOGNITION_ROLE_ARN
+    //     }
+    // };
     
     // try {
     //     const command = new StartFaceDetectionCommand(params);
